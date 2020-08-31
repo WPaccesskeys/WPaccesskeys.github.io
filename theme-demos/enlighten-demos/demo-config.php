8950745f5c53ae2c1bda6b45381d649a898215eb
WPaccesskeys <wp@access-keys.com>
2020-08-31 16:47:05 +0545

    updated

--- theme-demos/enlighten-demos/demo-config.php
+++ theme-demos/enlighten-demos/demo-config.php
@@ -7,19 +7,18 @@
 
 //add_filter('adi_demos_data','parallaxsome_pro_demo_config');
 
-function enlighten_demo_config(){
+//function enlighten_demo_config(){
 	
 		$git_url 		= 'https://raw.githubusercontent.com/WPaccesskeys/WPaccesskeys.github.io/master/theme-demos/enlighten-demos/';
 		$data = array(
 
 			'enlighten' => array(
-				'demo_name'			=> esc_html__('Enlighten Demo','access-demo-importer'),
+				'demo_name'			=> 'Enlighten Demo',
 				'xml_file'     		=> $git_url . 'enlighten/enlighten.xml',
 				'theme_settings' 	=> $git_url . 'enlighten/enlighten.dat',
 				'widgets_file'  	=> $git_url . 'enlighten/enlighten.wie',
 				'screen'			=> $git_url . 'enlighten/enlighten.png',
 				'preview_url'		=> 'https://demo.accesspressthemes.com/enlighten/',
-				'is_shop'			=> true,
 				'home_title'  		=> 'Home',
 				'blog_title'  		=> 'Blog',
 				'menus'				=> array(
@@ -28,18 +27,23 @@ function enlighten_demo_config(){
 				'required_plugins'  => array(
 					'free' => array(
 						array(
-							'slug'  	=> 'accesspress-twitter-feed',
-							'init'  	=> 'accesspress-twitter-feed/accesspress-twitter-feed.php',
-							'name'  	=> 'WP TFeed',
-						),
+								'slug'  	=> 'contact-form-7',
+								'init'  	=> 'contact-form-7/wp-contact-form-7.php',
+								'name'  	=> 'Contact Form 7',
+							),
 					),
 					'premium' => array(
-
 					),
 				),
 			),
 
 		);
 
-	return $data;
-}
+	/*return $data;
+}*/
+
+echo (json_encode($data));
+
+$fp = fopen('config.json', 'w');
+fwrite($fp, json_encode($data));
+fclose($fp);
