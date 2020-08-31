86c00977a6d6284587228e1dcca47737c23a0b22
WPaccesskeys <wp@access-keys.com>
2020-08-31 17:12:52 +0545

    update

---
+++ theme-demos/enlighten-demos/demo-config.php
@@ -0,0 +1,49 @@
+<?php 
+/**
+* Demo import configuration file
+*
+*
+*/
+
+//add_filter('adi_demos_data','parallaxsome_pro_demo_config');
+
+//function enlighten_demo_config(){
+	
+		$git_url 		= 'https://raw.githubusercontent.com/WPaccesskeys/WPaccesskeys.github.io/master/theme-demos/enlighten-demos/';
+		$data = array(
+
+			'enlighten' => array(
+				'demo_name'			=> 'Enlighten Demo',
+				'xml_file'     		=> $git_url . 'enlighten/enlighten.xml',
+				'theme_settings' 	=> $git_url . 'enlighten/enlighten.dat',
+				'widgets_file'  	=> $git_url . 'enlighten/enlighten.wie',
+				'screen'			=> $git_url . 'enlighten/enlighten.png',
+				'preview_url'		=> 'https://demo.accesspressthemes.com/enlighten/',
+				'home_title'  		=> 'Home',
+				'blog_title'  		=> 'Blog',
+				'menus'				=> array(
+										'primary' => 'menu 1',
+										),
+				'required_plugins'  => array(
+					'free' => array(
+						array(
+								'slug'  	=> 'contact-form-7',
+								'init'  	=> 'contact-form-7/wp-contact-form-7.php',
+								'name'  	=> 'Contact Form 7',
+							),
+					),
+					'premium' => array(
+					),
+				),
+			),
+
+		);
+
+	/*return $data;
+}*/
+
+echo (json_encode($data));
+
+$fp = fopen('config.json', 'w');
+fwrite($fp, json_encode($data));
+fclose($fp);
